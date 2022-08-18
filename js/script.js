e38985f76952a27ec3ad115e0181925cb01052a2
Abiey2579 <mbyahya2579@gmail.com>
2022-08-17 20:23:11 -0700

    Update some little thing

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* ============================== typing animation ============================ */
 var typed = new Typed(".typing",{
-    strings:["","Web Designer","Web Developer","Graphic Designer","YouTuber"],
+    strings:["","Web Designer","Web Developer","Graphic Designer","UI/UX Designer","Content Writer","Social Media Manager"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
